09da1354672378f63ddddf8fd3c948bdd39c8ca2
kimkihwan9505 <ghkdka9505@gmail.com>
2022-03-12 18:49:53 +0900

    modified css

---
+++ src/pages/ManageReview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const managereview =()=>{
+    return(
+        <>
+        리뷰 관리
+        </>
+    )
+}
+export default managereview
---
+++ src/pages/ManageUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const manageUser =()=>{
+    return(
+        <>
+        회원 관리
+        </>
+    )
+
+}
+export default manageUser
